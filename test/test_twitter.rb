b62838bd3c82c5288f0c04b9b8fb28d36c449823
jose antonio rodriguez leandro <alu0100696691@ull.edu.es>
2014-09-28 14:02:53 +0100

    Primeros test en carpeta test

---
+++ test/test_twitter.rb
@@ -0,0 +1,23 @@
+ENV['RACK_ENV'] = 'test'
+
+require 'minitest/autorun'
+require 'rack/test'
+require_relative 'twitter.rb'
+
+include Rack::Test::Methods
+	
+	def app
+		Sinatra::Application
+	end
+
+	def test_index
+    		get '/'
+    		assert last_response.ok?
+  	end
+
+	def test_title
+		get "/"
+		assert last_response.body.include?("<title>Contador Twitter</title>"), "El titulo debe ser 'Contador Twitter'"
+	end
+
+
