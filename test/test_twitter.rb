73602aa11d35d9381a5d914d36e8de98c8bdf083
jose antonio rodriguez leandro <alu0100696691@ull.edu.es>
2014-09-28 15:23:46 +0100

    mas test y solucionado fallos

--- test/test_twitter.rb
+++ test/test_twitter.rb
@@ -7,7 +7,7 @@ require_relative '../twitter.rb'
 include Rack::Test::Methods
 	
 	def app
-		Sinatra::Application
+		Sinatra::Application.new
 	end
 
 describe "STW2" do
@@ -18,16 +18,18 @@ describe "STW2" do
   	end
 
 	it "el titulo de la pagina es Contador Twitter?" do
-		get "/"
+		get '/'
 		assert last_response.body.include?("<title>Contador Twitter</title>"), "El titulo debe ser 'Contador Twitter'"
 	end
 
 	it "existe formulario donde preguntar nombre y cantidad de amigos a contar?" do
+		get '/'
 		assert last_response.body.include?("<p><b>Nombre de usuario en Twitter:</b></p>"), "El body debe contener nombre usuario."
 		assert last_response.body.include?("<p><b>¿Cuántos amigos desea ver? </b></p>"), "El body debe contener cuantos amigos."	
 	end
 
 	it "Pie de pagina contiene: © 2014 Sistemas y Tecnologias Web - ULL " do
+		get '/'
 		assert_match "<p>© 2014 Sistemas y Tecnologias Web - ULL</p>", last_response.body
 	end
 	
